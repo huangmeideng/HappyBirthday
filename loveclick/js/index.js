0226e79ca5bd636f4ec1ebf5fc7a9ad0f1429207
huangmeideng <huangmeideng@outlook.com>
2018-11-01 00:00:22 +0800

    music

--- loveclick/js/index.js
+++ loveclick/js/index.js
@@ -4,3 +4,12 @@ setInterval(function(){
     //document.body.onkeypress = function(){document.body.classList.toggle('liked')}
     document.body.classList.toggle('liked');
 },1000);
+
+function playMusic() {
+    var audioButton = document.getElementById("loveAudio");
+    if(audioButton.paused) {
+        audioButton.play();
+    } else{
+        audioButton.pause();
+    }
+}
